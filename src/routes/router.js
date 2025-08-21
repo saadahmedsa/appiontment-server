a9148cb8f02dfc51406b22d524f3a02bf1524748
Saad Ahmed <ss5099903@gmail.com>
2025-08-21 13:42:40 +0500

    route defined

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,16 +1,24 @@
 import express from "express"
 import { createDoctor, deleteDoctor, getDoctorById, getDoctors, updateDoctor } from "../controllers/Doctorcontroller.js"
+import { createAppointment, deleteAppointment, getAllAppointments, getMyAppointments, updateAppointment } from "../controllers/Appointmentcontroller.js"
 
 
 const router = express.Router()
 
-
+// doctor api
 router.post("/add", createDoctor)
 router.get("/all", getDoctors)
 router.get("/single/:id", getDoctorById)
 router.delete("/delete/:id", deleteDoctor)
 router.put("/edit/:id", updateDoctor)
+// appointment api
 
 
+// 📌 Create a new appointment
+router.post("/book", createAppointment);
+router.get("/allappointment", getAllAppointments);
+router.get("/my/:id", getMyAppointments);
+router.put("update/:id", updateAppointment);
+router.delete("delete/:id", deleteAppointment);
 
 export default router
