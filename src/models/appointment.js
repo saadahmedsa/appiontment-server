53c5acc40c07483ccb8acfda6b31f91a463927d4
Saad Ahmed <ss5099903@gmail.com>
2025-08-21 12:53:13 +0500

    appointment model add

---
+++ src/models/appointment.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    patientId: {
+      type: String, // Clerk userId
+      required: true,
+    },
+    name: {
+      type: String, // Clerk userId
+      required: true,
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    timeSlot: {
+      type: String, // Example: "10:00 AM - 10:30 AM"
+      required: true,
+    },
+    reason: {
+      type: String,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "cancelled", "completed"],
+      default: "pending",
+    },
+    notes: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Appointment", appointmentSchema);
